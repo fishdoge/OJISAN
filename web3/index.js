9ae5eb76a3b9754819bd64d4e396004e90b37e25
unknown <ru19wu06@gmail.com>
2022-03-26 00:07:31 +0800

    update icon

--- web3/index.js
+++ web3/index.js
@@ -4,21 +4,29 @@ async function login_In_button(){
 }
 
 async function setCollent(){
-    let account = await ethereum.request({ method: 'eth_requestAccounts' });
-   
-    coinbase = account[0];
-   
+    let coinbase = await web3.eth.getCoinbase();
+
+    //coinbase = account[0];
+
     let ETHbalance = await web3.eth.getBalance(coinbase);
     ETHbalance = web3.utils.fromWei(ETHbalance, 'ether');
 
     let add = coinbase.substring(0,12)
-    add = add +"....";
+
+    let network = await web3.eth.net.getNetworkType();
+
+    if(network != 'rinkeby'){
+        console.log("please change to main net");
+
+        alert("請切換至 rinkeby 測試網");
+    }
+
 
     console.log(add);
     $("#tes").text(add);
 
-    $("#Addr").text(coinbase);
-    
+    $("#Addr").text(add);
+
 }
 
 
