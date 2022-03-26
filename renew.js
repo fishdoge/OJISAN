9daa87253009fdd25bac1be45ca872bf4566282c
unknown <ru19wu06@gmail.com>
2022-03-26 10:04:20 +0800

    update metadata

--- renew.js
+++ renew.js
@@ -9,7 +9,7 @@ for(var a=1;a<=200;a++){
 
     var names = `OGSAN #${a}`;
     var description = 'The OGSAN Club member';
-    var image = 'https://ipfs.io/ipfs/QmVJgzbBgHVfcrY8fCHradsibAECTtz8omPs13axfNSGkx';
+    var image = 'https://gateway.pinata.cloud/ipfs/Qmf2CyNxjGZCJZ8K6a4FF9V5Z2D2TyxgqGSLfNBHHnTujh';
 
 
     var Attributes = [
